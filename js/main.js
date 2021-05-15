642a5feb8ac036f141cd828148e00ffbe27c847e
yan <yanmalinowskij@gmail.com>
2021-05-15 22:38:42 +0100

    js fix

--- js/main.js
+++ js/main.js
@@ -2,19 +2,19 @@ function changeFunction() {
   var image = document.getElementById("menu_button");
   if (image.src.match("icon-hamburger")) {
     image.src = "images/icon-close.svg";
-    image.style.marginTop = "60px"
-    image.style.marginRight = "27px"
+    image.style.marginTop = "60px";
+    image.style.marginRight = "27px";
   } else {
     image.src = "images/icon-hamburger.svg";
-    image.style.marginTop = "63px"
-    image.style.marginRight = "24px"
+    image.style.marginTop = "63px";
+    image.style.marginRight = "24px";
 
   }
 
   var showOrHide = document.getElementById("mobile_menu");
   if (showOrHide.className === "responsive_menu") {
-    showOrHide.className += " show"
+    showOrHide.className += " show";
   } else {
-    showOrHide.className = "responsive_menu"
+    showOrHide.className = "responsive_menu";
   }
 }
